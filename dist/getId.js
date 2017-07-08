d611064a0b99a4df6f6c2ab8a605222697e30fd9
Ollie Ayre <oliver.ayre@oddschecker.com>
2017-07-08 18:15:23 +0100

    remove dist and add to gitignore

--- dist/getId.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function getId(id) {
-    var i = id.replace("#", String());
-    var el = document.getElementById(i);
-    return el;
-}
-exports.getId = getId;
--- dist/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var getClass_1 = require("./getClass");
-exports.getClass = getClass_1.getClass;
-var getId_1 = require("./getId");
-exports.getId = getId_1.getId;
